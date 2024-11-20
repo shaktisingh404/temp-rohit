2e38bd9d9e28882048c4b871d7e3f1cc58dddb36
Rohit Kumar <rk8106263@gmail.com>
2024-11-20 21:21:43 +0530

    Done

---
+++ backend/lib/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ₹{conn.connection.host}`);
+  } catch (error) {
+    console.log("Error connecting to MONGODB", error.message);
+    process.exit(1);
+  }
+};
+
+// here ruppe will change
